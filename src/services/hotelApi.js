29885350ab35dd8ac02632677b28bcf00291f44a
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-12-02 16:38:54 +0200

    get hotels from json-server, imitateImg, show Hotel card

--- src/services/hotelApi.js
+++ src/services/hotelApi.js
@@ -1,4 +1,5 @@
 import { API_URL, ImagesNumber, ImagesPath } from '../../config';
+import { imitateImg } from './imitateImg';
 
 const getHotels = async (search) => {
     const response = await fetch(`${API_URL}/hotels?name_like=${search}`);
@@ -10,25 +11,6 @@ const getHotel = async (id) => {
     const hotel = { name: 'hotel 1', stars: 5 };
 
     return hotel;
-};
-
-const imitateImg = (hotels) => {
-    const updatedHotels = hotels.map((hotel) => {
-        const imgCount = hotel.images.length;
-        hotel.images = hotel.images.map((img) => {
-            img = getNextRandomImg();
-            return img;
-        });
-        return hotel;
-    });
-    return updatedHotels;
-};
-const getNextRandomImg = () => {
-    const image = `${ImagesPath}/${Math.floor(
-        Math.random() * ImagesNumber
-    )}.jpg`;
-    console.log(image);
-    return image;
 };
 
 export { getHotels, getHotel };
