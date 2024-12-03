4b5081ea30b8469b2e8788ba8de467492f5d1e0e
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-12-03 21:09:06 +0200

    refactoring HotelItem card

--- src/component/hotel/HotelItem.jsx
+++ src/component/hotel/HotelItem.jsx
@@ -5,22 +5,15 @@ import './hotel-item.css';
 function HotelItem(props) {
     const { hotel: h } = props;
     return (
-        <div className='col s12 m7'>
-            <div className='card horizontal'>
-                <div className='card-image'>
-                    <img src={h.images[0]} />
-                </div>
-                <div className='card-stacked'>
-                    <div className='card-content'>
-                        <div className='p1-item-name'>{h.name}</div>
-                        <HotelStars stars={h.stars} />
-                        <div className='p1-item-location'>{h.location}</div>
-                        <HotelFeatures features={h.features} />
-                    </div>
-                    <div className='card-action'>
-                        <a href='#'>This is a link</a>
-                    </div>
-                </div>
+        <div className='p1-item'>
+            <div>
+                <img src={h.images[0]} />
+            </div>
+            <div>
+                <div className='p1-item-name'>{h.name}</div>
+                <HotelStars stars={h.stars} />
+                <div className='p1-item-location'>{h.location}</div>
+                <HotelFeatures features={h.features} />
             </div>
         </div>
     );
