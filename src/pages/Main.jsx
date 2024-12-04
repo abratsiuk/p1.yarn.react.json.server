f24c4bef01ca66e1d16cc4a4a77545fbf16cbeed
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-12-04 12:28:44 +0200

    add Debug Wrap

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,6 +3,7 @@ import { Hotels } from '../component/Hotels';
 import { Preloader } from '../component/Preloader';
 import { Search } from '../component/Search';
 import { getHotels } from '../services/hotelApi';
+import { DebugWrap } from '../component/utils/DebugWrap';
 
 function Main() {
     const [hotels, setHotels] = useState([]);
@@ -28,7 +29,9 @@ function Main() {
     return (
         <>
             <Search reloadHotels={reloadHotels} />
-            <img src='/src/component/hotel/docs/hotel-card-example.jpg' />
+            <DebugWrap name='image: hotel-card-example'>
+                <img src='/src/component/hotel/docs/hotel-card-example.jpg' />
+            </DebugWrap>
 
             {loading ? <Preloader /> : <Hotels hotels={hotels} />}
         </>
